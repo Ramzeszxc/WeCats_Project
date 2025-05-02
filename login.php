8e0530ef31c201f02ca3694b473b7c49accd2842
Ramzeszxc <michaelangelou.quinit@g.msuiit.edu.ph>
2025-05-02 05:29:00 +0000

    Tiwason na tani, next week na ang deadline

--- login.php
+++ login.php
@@ -2,13 +2,20 @@
 // Start the session
 session_start();
 
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Dummy user credentials for demonstration
 $valid_email = "michaelangelou.quinit@g.msuiit.edu.ph";
 $valid_password = "sept232005";
 
 // Get the submitted form data
-$email = $_POST['email'];
-$password = $_POST['password'];
+$email = $_POST['Email'] ?? '';
+$password = $_POST['Password'] ?? '';
+
+// Debugging output
+var_dump($email, $password);
 
 // Check if the credentials are valid
 if ($email === $valid_email && $password === $valid_password) {
